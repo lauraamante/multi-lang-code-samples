cddd1efdf96f8193987610bc6cc310ba4f86dda6
Laura Amante <lauraoliveira.amante@gmail.com>
2025-10-28 17:20:46 -0300

    Update and rename calculator.cpp to calculator-switch.cpp

---
+++ calculator-switch.cpp
@@ -0,0 +1,55 @@
+#Testando programa com switch case
+
+include <iostream>
+using namespace std;
+
+#soma
+float sum (float a, float b) {
+  return a + b;
+}
+
+#subtracao
+float subtraction (float a, float b){
+  return a + b;
+}
+#multiplicacao
+float multiply (float a, float b){
+  return a * b;
+}
+#divisao
+float divide (float a, float b){
+  if (b == 0){
+    cout << "Erro. Impossível dividir por zero." << endl;
+  }
+
+return a/b;
+
+}
+#main
+int main(){
+float x, y, resultado;
+char op;
+  cout<<"Calculadora básica"<<endl;
+
+cout <<"\nDigite o primeiro número:\n";
+cin >> x;
+
+cout <<"\nDigite o operador:\n";
+cin >> op;
+
+cout <<"\nDigite o segundo número:\n";
+cin >> y;
+
+
+switch (op){
+  case '+': resultado = sum(x,y); break;
+  case '-': resultado = subtract(x,y); break;
+  case '/': resultado = divide(x,y); break;
+  case '*': resultado = multiply(x,y); break;
+  default:
+    cout << "operador invalido." << endl;
+}
+
+  cout << "|  Resultado:  |\n" << resultado <<endl;
+
+}
